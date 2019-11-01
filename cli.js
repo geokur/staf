344becc7d7b18ee7250ccba70844d663586fb9db
Georgii Kurtanidze <gku@ciklum.com>
2019-11-02 00:02:14 +0200

    refactor cli to print errors

--- cli.js
+++ cli.js
@@ -2,21 +2,31 @@
 const path = require('path')
 const { run } = require('./src/runner')
 const defaultConfig = require('./src/default-config')
+const version = require('./package').version
+const { printHeader, printFooter, printLine, cutStack } = require('./src/util')
 
-console.log('==========================================')
-console.log('|  Simple Test Automation Framework 1.0  |')
-console.log('==========================================')
+printHeader(version)
 
 if (process.argv.length === 3 ) {
     const configFile = process.argv[2]
     const configPath = path.resolve(configFile)
     const testConfig = require(configPath)
     const config = Object.assign({}, defaultConfig, testConfig)
-    console.log('Running config:')
-    console.log('  %s', configPath)
-    console.log('------------------------------------------')
-    run(config).then(status => process.exit(status))
+    console.log('Running config:', configPath)
+    printLine('-')
+    run(config).then(
+        status => {
+            printFooter(status)
+            process.exit(status)
+        }, 
+        error => {
+            console.log(`\x1b[31m${cutStack(error)}\x1b[0m`)
+            printFooter(1)
+            process.exit(1)
+        }
+    )
 } else {
     console.log('Usage:')
     console.log('  simple <config-file.js>')
 }
+
